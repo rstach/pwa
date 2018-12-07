82ac84db8711bf4f2a5c45fd616bd9e20b1e8246
rominastach <rominastach@gmail.com>
2018-12-07 08:34:03 -0300

    primer commit de la pwa

---
+++ sw.js
@@ -0,0 +1,84 @@
+//asignar nombre y version de la cache
+const CACHE_NAME = 'v1_cache_victor_robles_pwa';
+
+//ficheros a cachear en la aplicacion
+var urlsToCache = [
+'./',
+'./css/styles.css',
+'./img/favicon.png',
+'./img/1.png',
+'./img/2.png',
+'./img/3.png',
+'./img/4.png',
+'./img/5.png',
+'./img/6.png',
+'./img/facebook.png',
+'./img/instagram.png',
+'./img/twitter.png',
+'./img/favicon-1024.png',
+'./img/favicon-512.png',
+'./img/favicon-384.png',
+'./img/favicon-256.png',
+'./img/favicon-192.png',
+'./img/favicon-128.png',
+'./img/favicon-96.png',
+'./img/favicon-64.png',
+'./img/favicon-32.png',
+'./img/favicon-16.png',
+];
+
+//Evento install
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache =>{
+                return cache.addAll(urlsToCache)
+                    .then(()=>{
+                        self.skipWaiting();
+                    });
+                    
+                    })
+            .catch (err => console.log('No se ha registrado el cache', err))
+    );
+});
+
+//Evento ActivaTE que la app funcione sin conexion
+
+self.addEventListener('activate', e=>{
+    const cacheWhitelist =[CACHE_NAME];
+
+    e.waitUntil(
+        caches.keys()
+            .then(cacheNames =>{
+                return Promise.all(
+                    cacheNames.map(cacheName =>{
+                        if (cacheWhitelist.indexOf(cacheName)=== -1){
+                            //borrar elementos de la cahe que no se necesitan
+                             return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
+            .then(()=>{
+                //activa la cache
+                self.clients.claim();            
+            })
+    );
+
+});
+
+//Evento fetch
+self.addEventListener('fetch', e=>{
+
+    e.respondWith(
+        caches.match(e.request)
+            .then (res=>{
+                if(res){
+                    //devuelvo datps desde cache
+                    return res;
+                }
+                return fetch(e.request);
+            })
+    );
+});
